a77bde1c2f1baef97237250b6bed3aad9c8129ab
kimkichan-1 <20201536@daejin.ac.kr>
2025-07-03 15:58:35 +0900

    Update: HTML and JS files

--- player.js
+++ player.js
@@ -2,7 +2,6 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.124/build/three.mod
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.124/examples/jsm/loaders/GLTFLoader.js';
 
 export const player = (() => {
-
     class Player {
         constructor(params) {
             this.position_ = new THREE.Vector3(0, 0, 0);
@@ -18,8 +17,24 @@ export const player = (() => {
                 backward: false,
                 left: false,
                 right: false,
-                shift: false, // Shift 키 추가
+                shift: false,
             };
+            this.jumpPower_ = 12;
+            this.gravity_ = -30;
+            this.isJumping_ = false;
+            this.velocityY_ = 0;
+            this.jumpSpeed_ = 0.5;
+
+            // Roll(구르기) 관련 변수
+            this.isRolling_ = false;
+            this.rollDuration_ = 0.5;
+            this.rollTimer_ = 0;
+            this.rollSpeed_ = 18;
+            this.rollDirection_ = new THREE.Vector3(0, 0, 0);
+
+            // === 쿨타임 관련 변수 추가 ===
+            this.rollCooldown_ = 1.0;    // 쿨타임(초)
+            this.rollCooldownTimer_ = 0; // 남은 쿨타임
 
             this.LoadModel_();
             this.InitInput_();
@@ -37,8 +52,48 @@ export const player = (() => {
                 case 'KeyA': this.keys_.left = true; break;
                 case 'KeyD': this.keys_.right = true; break;
                 case 'ShiftLeft':
-                case 'ShiftRight':
-                    this.keys_.shift = true; break;
+                case 'ShiftRight': this.keys_.shift = true; break;
+                case 'KeyK':
+                    if (!this.isJumping_ && !this.isRolling_) {
+                        this.isJumping_ = true;
+                        this.velocityY_ = this.jumpPower_;
+                        this.SetAnimation_('Jump');
+                    }
+                    break;
+                case 'KeyL':
+                    // === 쿨타임 체크 추가 ===
+                    if (
+                        !this.isJumping_ &&
+                        !this.isRolling_ &&
+                        this.animations_['Roll'] &&
+                        this.rollCooldownTimer_ <= 0 // 쿨타임이 끝났을 때만 롤 가능
+                    ) {
+                        this.isRolling_ = true;
+                        this.rollTimer_ = this.rollDuration_;
+
+                        // 롤 방향 결정
+                        const moveDir = new THREE.Vector3();
+                        if (this.keys_.forward) moveDir.z -= 1;
+                        if (this.keys_.backward) moveDir.z += 1;
+                        if (this.keys_.left) moveDir.x -= 1;
+                        if (this.keys_.right) moveDir.x += 1;
+
+                        if (moveDir.lengthSq() === 0) {
+                            // 이동 입력이 없으면 캐릭터가 바라보는 방향(전방)으로 롤
+                            this.mesh_.getWorldDirection(moveDir);
+                            moveDir.y = 0;
+                            moveDir.normalize();
+                        } else {
+                            moveDir.normalize().applyAxisAngle(new THREE.Vector3(0, 1, 0), this.lastRotationAngle_ || 0);
+                        }
+                        this.rollDirection_.copy(moveDir);
+
+                        this.SetAnimation_('Roll');
+
+                        // === 롤 시작 시 쿨타임 설정 ===
+                        this.rollCooldownTimer_ = this.rollCooldown_;
+                    }
+                    break;
             }
         }
 
@@ -49,15 +104,14 @@ export const player = (() => {
                 case 'KeyA': this.keys_.left = false; break;
                 case 'KeyD': this.keys_.right = false; break;
                 case 'ShiftLeft':
-                case 'ShiftRight':
-                    this.keys_.shift = false; break;
+                case 'ShiftRight': this.keys_.shift = false; break;
             }
         }
 
         LoadModel_() {
             const loader = new GLTFLoader();
             loader.setPath('./resources/Ultimate Animated Character Pack - Nov 2019/glTF/');
-            loader.load('BaseCharacter.gltf', (gltf) => {
+            loader.load('Zombie_Male.gltf', (gltf) => {
                 const model = gltf.scene;
                 model.scale.setScalar(1);
                 model.quaternion.setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI / 2);
@@ -75,11 +129,9 @@ export const player = (() => {
                 });
 
                 this.mixer_ = new THREE.AnimationMixer(model);
-
                 for (const clip of gltf.animations) {
                     this.animations_[clip.name] = this.mixer_.clipAction(clip);
                 }
-
                 this.SetAnimation_('Idle');
                 console.log("애니메이션 목록:", Object.keys(this.animations_));
             });
@@ -87,57 +139,104 @@ export const player = (() => {
 
         SetAnimation_(name) {
             if (this.currentAction_ === this.animations_[name]) return;
-
             if (this.currentAction_) {
                 this.currentAction_.fadeOut(0.3);
             }
             this.currentAction_ = this.animations_[name];
             if (this.currentAction_) {
                 this.currentAction_.reset().fadeIn(0.3).play();
+                if (name === 'Jump') {
+                    this.currentAction_.setLoop(THREE.LoopOnce);
+                    this.currentAction_.clampWhenFinished = true;
+                    this.currentAction_.time = 0.25;
+                    this.currentAction_.timeScale = this.jumpSpeed_;
+                } else if (name === 'Roll') {
+                    this.currentAction_.setLoop(THREE.LoopOnce);
+                    this.currentAction_.clampWhenFinished = true;
+                    this.currentAction_.time = 0.0;
+                    this.currentAction_.timeScale = 1.2;
+                } else {
+                    this.currentAction_.timeScale = 1.0;
+                }
             }
         }
 
         Update(timeElapsed, rotationAngle = 0) {
             if (!this.mesh_) return;
+            this.lastRotationAngle_ = rotationAngle;
 
-            const velocity = new THREE.Vector3();
-            const forward = new THREE.Vector3(0, 0, -1);
-            const right = new THREE.Vector3(1, 0, 0);
+            // === 쿨타임 타이머 감소 ===
+            if (this.rollCooldownTimer_ > 0) {
+                this.rollCooldownTimer_ -= timeElapsed;
+                if (this.rollCooldownTimer_ < 0) this.rollCooldownTimer_ = 0;
+            }
 
-            if (this.keys_.forward) velocity.add(forward);
-            if (this.keys_.backward) velocity.sub(forward);
-            if (this.keys_.left) velocity.sub(right);
-            if (this.keys_.right) velocity.add(right);
+            if (this.isRolling_) {
+                this.rollTimer_ -= timeElapsed;
+                const rollMove = this.rollDirection_.clone().multiplyScalar(this.rollSpeed_ * timeElapsed);
+                this.position_.add(rollMove);
 
-            // 방향 회전 적용
-            velocity.applyAxisAngle(new THREE.Vector3(0, 1, 0), rotationAngle);
-
-            const isMoving = this.keys_.forward || this.keys_.backward || this.keys_.left || this.keys_.right;
-            const isRunning = isMoving && this.keys_.shift;
-            const moveSpeed = isRunning ? this.speed_ * 2 : this.speed_;
-
-            velocity.normalize().multiplyScalar(moveSpeed * timeElapsed);
-            this.position_.add(velocity);
-
-            if (velocity.length() > 0.01) {
-                this.SetAnimation_(isRunning ? 'Run' : 'Walk');
-
-                const angle = Math.atan2(velocity.x, velocity.z);
-                const targetQuaternion = new THREE.Quaternion().setFromAxisAngle(
-                    new THREE.Vector3(0, 1, 0), angle
-                );
-                this.mesh_.quaternion.slerp(targetQuaternion, 0.1);
+                if (this.rollTimer_ <= 0) {
+                    this.isRolling_ = false;
+                    const isMoving = this.keys_.forward || this.keys_.backward || this.keys_.left || this.keys_.right;
+                    const isRunning = isMoving && this.keys_.shift;
+                    if (isMoving) {
+                        this.SetAnimation_(isRunning ? 'Run' : 'Walk');
+                    } else {
+                        this.SetAnimation_('Idle');
+                    }
+                }
             } else {
-                this.SetAnimation_('Idle');
+                const velocity = new THREE.Vector3();
+                const forward = new THREE.Vector3(0, 0, -1);
+                const right = new THREE.Vector3(1, 0, 0);
+
+                if (this.keys_.forward) velocity.add(forward);
+                if (this.keys_.backward) velocity.sub(forward);
+                if (this.keys_.left) velocity.sub(right);
+                if (this.keys_.right) velocity.add(right);
+
+                velocity.applyAxisAngle(new THREE.Vector3(0, 1, 0), rotationAngle);
+
+                const isMoving = this.keys_.forward || this.keys_.backward || this.keys_.left || this.keys_.right;
+                const isRunning = isMoving && this.keys_.shift;
+                const moveSpeed = isRunning ? this.speed_ * 2 : this.speed_;
+
+                velocity.normalize().multiplyScalar(moveSpeed * timeElapsed);
+                this.position_.add(velocity);
+
+                this.velocityY_ += this.gravity_ * timeElapsed;
+                this.position_.y += this.velocityY_ * timeElapsed;
+
+                if (this.position_.y <= 0) {
+                    this.position_.y = 0;
+                    this.velocityY_ = 0;
+                    if (isMoving) {
+                        this.SetAnimation_(isRunning ? 'Run' : 'Walk');
+                    } else {
+                        this.SetAnimation_('Idle');
+                    }
+                    this.isJumping_ = false;
+                }
+
+                if (this.position_.y > 0 && this.isJumping_) {
+                    this.SetAnimation_('Jump');
+                }
+
+                if (velocity.length() > 0.01) {
+                    const angle = Math.atan2(velocity.x, velocity.z);
+                    const targetQuaternion = new THREE.Quaternion().setFromAxisAngle(
+                        new THREE.Vector3(0, 1, 0), angle
+                    );
+                    this.mesh_.quaternion.slerp(targetQuaternion, 0.3);
+                }
             }
 
             this.mesh_.position.copy(this.position_);
-
             if (this.mixer_) {
                 this.mixer_.update(timeElapsed);
             }
         }
-
     }
 
     return {
